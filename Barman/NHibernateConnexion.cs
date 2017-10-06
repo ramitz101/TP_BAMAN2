9a1f1ba9bcbf5764346d2e53ae144d01e9fb5833
ramitz101 <ramitz101@gmail.com>
2017-10-06 15:48:48 -0400

    CLASS MAP

---
+++ Barman/NHibernateConnexion.cs
@@ -0,0 +1,45 @@
+﻿using FluentNHibernate.Cfg;
+using NHibernate;
+using NHibernate.Cfg;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Barman
+{
+    public class NHibernateConnexion
+    {
+        private static ISessionFactory _sessionFactory;
+        private static readonly bool IS_FLUENT = true;
+
+        static NHibernateConnexion()
+        {
+
+            if (IS_FLUENT)
+            {
+                _sessionFactory = Fluently.Configure().Mappings(m => m.FluentMappings.AddFromAssemblyOf<NHibernateConnexion>()).BuildSessionFactory();
+            }
+            else
+            {
+                _sessionFactory = new Configuration().AddAssembly(typeof(NHibernateConnexion).Assembly).BuildSessionFactory();
+            }
+
+        }
+
+        private static ISessionFactory SessionFactory
+        {
+            get
+            {
+                return _sessionFactory;
+            }
+        }
+
+        public static ISession OpenSession()
+        {
+            return SessionFactory.OpenSession();
+
+        }
+    }
+}
